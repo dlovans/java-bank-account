707736ca1fbed1fce8c8647af7032b1999782226
Dlovan Sharif <dlovan.s.sharif@gmail.com>
2024-09-16 02:04:44 +0200

    Instantiate an instance of Account

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner input = new Scanner(System.in);
+        Account account;
+        double balance;
+        String name;
+
+        System.out.println("Enter balance amount: ");
+        balance = input.nextDouble();
+        input.nextLine();
+
+        System.out.println("Enter account owner name: ");
+        name = input.nextLine();
+
+        account = new Account(balance, name);
+
+        System.out.println(account);
+
+        account.deposit(500);
+        System.out.println(account);
+        account.withdraw(100);
+        System.out.println(account);
+        account.setName("llama");
+        System.out.println(account);
     }
 }
